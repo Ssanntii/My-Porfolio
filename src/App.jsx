31be60664e2f039d64f94447edf4b00a4a57a845
Santiago Ruiz Diaz | UAA-Sistemas <srdiaz@uaa.com.ar>
2025-10-01 12:00:24 -0300

    My portfolio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import Hero from "./components/Hero"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration:1000
+    })
+  }, [])
+
+  return (
+    <main className="bg-gray-950">
+     <Hero/>
+    </main>
+  )
+}
+
+export default App
